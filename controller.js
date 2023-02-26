5b0737834939b041aa510a407a9083355b131561
Andri Wijaya <echo.hahihuheho8899@gmail.com>
2023-02-26 18:35:06 +0700

    berhasilkan menampilkan data berdasarkan id

--- controller.js
+++ controller.js
@@ -11,9 +11,22 @@ exports.index = function(req, res){
 exports.tampilsemuamahasiswa = function(req, res){
     connection.query("select * from mahasiswa", function(error, rows, fields){
         if(error){
-            connection.log(error);        
+            console.log(error);        
         }else{
-            response.ok(rows, res)
+            response.ok(rows, res);
+        }
+    });
+};
+
+//menampilkan semua data mahasiswa berdasarkan id
+exports.tampilberdasarkanid = function(req, res){
+    let id = req.params.id;
+    connection.query("select * from mahasiswa where id_mahasiswa = ?", [id],
+    function(error, rows, fields){
+        if(error){
+            console.log(error);
+        }else {
+            response.ok(rows, res);
         }
     });
 };
--- routes.js
+++ routes.js
@@ -4,8 +4,11 @@ module.exports = function(app){
     var jsonku = require('./controller');
 
     app.route('/')
-    .get(jsonku.index);
+        .get(jsonku.index);
 
     app.route('/tampil')
-    .get(jsonku.tampilsemuamahasiswa);
+        .get(jsonku.tampilsemuamahasiswa);
+
+    app.route('/tampil/:id')
+        .get(jsonku.tampilberdasarkanid);
 }
