9eb0281b13c3848d554ee821787543af4d1635c4
Andri Wijaya <echo.hahihuheho8899@gmail.com>
2023-02-26 20:01:03 +0700

    berhasil ubah data

--- controller.js
+++ controller.js
@@ -47,3 +47,20 @@ exports.tambahmahasiswa = function(req, res){
         }
     });
 };
+
+//mengubah data berdasarkan id
+exports.ubahmahasiswa = function(req, res){
+    var id = req.body.id_mahasiswa;
+    var nim = req.body.nim;
+    var nama = req.body.nama;
+    var jurusan = req.body.jurusan;
+
+    connection.query('update mahasiswa set nim=?, nama=?, jurusan=? where id_mahasiswa=?', [nim,nama,jurusan, id], 
+    function(error, rows, fields){
+        if(error){
+            console.log(error);
+        }else {
+            response.ok('berhasil uba data', res)
+        }
+    });
+};
